735e0a88b80dd094897df7752eb6e8839b1385c3
ikostan <superegorius@gmail.com>
2017-12-10 21:25:47 -0800

    HsqlDB - connection is ready

--- src/com/ikostan/javatraining/ConnectHsqlDb.java
+++ src/com/ikostan/javatraining/ConnectHsqlDb.java
@@ -2,7 +2,11 @@ package com.ikostan.javatraining;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.jdbc.ResultSetImpl;
 
 public class ConnectHsqlDb {
 
@@ -15,21 +19,46 @@ public class ConnectHsqlDb {
 		//Class.forName("com.mysql.jdbc.Driver"); //There is no need for that since we work with Java 6 or 7
 		
 		Connection connection = null;
+		Statement statement = null;
+		ResultSet resultSet = null;
 		
 		try 
 		{
+			//Connection object
 			connection = DriverManager.getConnection(CONN_STRING, USERNAME, PASSWORD);
 			System.out.println("DB (Hsql) connection established");
+			
+			//Read only result set
+			statement = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY); 
+			
+			//SQL query
+			resultSet = statement.executeQuery("SELECT * FROM states");
+			
+			//Get last row index
+			resultSet.last();
+			
+			//Print total number of rows
+			System.out.println("Number of rows: " + resultSet.getRow());
 		} 
 		catch (SQLException e) 
 		{			
-			System.err.println(e);
+			System.err.println(e); //Print error message
 		}
 		finally
 		{			
+			if (resultSet != null)
+			{				
+				resultSet.close(); //Close resultSet
+			}
+			
+			if (statement != null)
+			{				
+				statement.close(); //Close statement
+			}
+			
 			if (connection != null)
 			{				
-				connection.close();
+				connection.close(); //Close connection
 			}
 		}		
 	}
